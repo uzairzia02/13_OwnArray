74b31ad46621b7014f72f5d3054dc9bbc7a4eab9
uzairzia <uzair.governorsindh@gmail.com>
2024-03-01 08:53:48 +0500

    initial commit

---
+++ Own_Array.js
@@ -0,0 +1,5 @@
+"use strict";
+let own_array = ['bike', 'car', 'bus', 'train', 'plane'];
+for (let i = 0; i < own_array.length; i++) {
+    console.log('I would like to own a ' + own_array[i]);
+}
---
+++ Own_Array.ts
@@ -0,0 +1,5 @@
+let own_array: string[] = ['bike', 'car', 'bus', 'train', 'plane'];
+
+for(let i=0; i<own_array.length; i++){
+    console.log('I would like to own a ' + own_array[i]);
+}
